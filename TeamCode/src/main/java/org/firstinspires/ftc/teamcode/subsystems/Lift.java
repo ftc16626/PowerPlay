5c3116de27267cebf143354a386464c92c6701c6
AriDela <55457916+Nitr0gue@users.noreply.github.com>
2022-10-13 21:32:39 -0500

    Updates
    Added directories
    Added empty subsystems
    Added SampleMecanumDrive with DriveConstants
    Added Encoder
    Added unfinished Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+public class Lift {
+
+/*two motors w/ encoders, both same speed and direction always,
+start out running 10% power for safety, needs to hold position, may add limit switches to stop motion at top/bottom */
+
+}
